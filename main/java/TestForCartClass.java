c8e5203b8d6c9f36b7b57732821c6a95071b8a2b
LenousikK <liloo-19@yandex.ru>
2019-03-11 13:41:43 +0300

    Task10. Solution

---
+++ main/java/TestForCartClass.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestForCartClass extends BeforeAfter {
+
+    @Test
+    void scenarioGetCartName() {
+
+        assertEquals("new-cart", newCart.getCartName());
+
+    }
+}
---
+++ main/java/TestForJsonParserClassWriteTo.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestForJsonParserClassWriteTo extends BeforeAfter {
+
+    @Test
+
+    public void scenarioWriteToValidData() {
+
+        parser.writeToFile(createCart());
+
+        File folder = new File("src/main/resources/");
+        File[] arrayOfFilesInResourcesFolder = folder.listFiles();
+        ArrayList<File> arrayListOfFilesInResourcesFolder = new ArrayList<File>(Arrays.asList(arrayOfFilesInResourcesFolder));
+
+        for (int i = 0; i < arrayListOfFilesInResourcesFolder.size(); i++) {
+            if (arrayListOfFilesInResourcesFolder.get(i).isFile()) {
+
+                if (arrayListOfFilesInResourcesFolder.get(i).getName().equals("new cart.json")) {
+                    assertTrue(true);
+                }
+            }
+
+        }
+    }
+}
+
